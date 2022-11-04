12abb355d13164d767344fbc2a2068dc8564472e
Sara Choi <sarachoi10@hotmail.com>
2022-11-04 02:55:38 +0000

    Completed user-can-create-an-entry.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('data-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+window.addEventListener('beforeunload', event => {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+});
